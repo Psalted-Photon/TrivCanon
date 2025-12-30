2c8a009892f1d63d9a9954cd53cac0cc33112008
Marlowe <marlowescotts@gmail.com>
2025-12-30 15:27:17 -0600

    Increase PWA cache size limit to 5MB for title image

--- app/vite.config.js
+++ app/vite.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB limit
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
